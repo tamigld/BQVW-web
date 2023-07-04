48e90516e4c9e8be8a4b99f2c1d7693839445295
tamigld <thami.galves@outlook.com>
2023-07-03 22:48:36 -0300

    backup, mas perdemos commits. ver print -ultimos_commits.png-.

---
+++ Cadastro/script.js
@@ -0,0 +1,7 @@
+function on() {
+    document.getElementById("mostrar_overlay").style.display = "flex";
+  }
+  
+  function off() {
+    document.getElementById("mostrar_overlay").style.display = "none";
+  }
